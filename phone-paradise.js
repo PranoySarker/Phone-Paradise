4e18dce36618f31d730de1f3abfaddc683f79138
PranoySarker <pranoysarker01@gmail.com>
2022-03-01 03:33:28 +0600

    cards completed

--- phone-paradise.js
+++ phone-paradise.js
@@ -1,17 +1,42 @@
+//  onclick event funcion 
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
-    const url = 'https://openapi.programming-hero.com/api/phones?';
+    searchField.value = '';
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data))
 }
 
-const displayPhone = phones => {
+//display phones results
+const displayPhone = data => {
+    // console.log(data);
+    const limit = 20;
+    const phones = data.slice(0, limit);
     console.log(phones);
     const searchResult = document.getElementById('search-result');
-    phones.forEach(element => {
-        console.log(element);
-    });
+    searchResult.textContent = '';
+    if (phones.length == 0) {
+        searchResult.innerHTML = `<h1 class='message'>There is no such result</h1>`
+    }
+    else {
+
+        phones.forEach(phone => {
+            console.log(phone);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `<div class="card">
+                            <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <p class="card-text">${phone.brand}</p>
+                            <button class="btn btn-primary">Details</button>
+                        </div>
+                    </div>`;
+            searchResult.appendChild(div);
+        });
+    }
 }
